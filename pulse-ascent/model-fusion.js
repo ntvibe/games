15a51c6ef6a553defbe7c1fadb93c53cb65d94de
ntvibe <hakerche@gmail.com>
2026-08-20 10:54:38 +0300

    Load boss failure animation with model fusion

--- pulse-ascent/model-fusion.js
+++ pulse-ascent/model-fusion.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import './boss-damage-staging.js';
+import './boss-failure-animation.js';
 
 const ROOT='./assets/models/cc0/';
 const FILES={machine:'factory-machine.glb',pipe:'factory-pipe.glb',conveyor:'factory-conveyor.glb',tank:'factory-tank.glb',rifle:'blaster-rifle.glb',crate:'blaster-crate.glb'};
